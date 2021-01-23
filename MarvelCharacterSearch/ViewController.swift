663ca8855c97ad77443828c228824e0f065715c2
SemaDevpy <syimykdev@gmail.com>
2021-01-23 14:08:59 +0600

    Initial Commit

---
+++ MarvelCharacterSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarvelCharacterSearch
+//
+//  Created by Syimyk on 1/23/21.
+//  Copyright © 2021 Syimyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
